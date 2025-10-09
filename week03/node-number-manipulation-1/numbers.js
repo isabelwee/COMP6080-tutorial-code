204976906edd27dd4ea65ccfbb542083148fbb24
Isabel Wee <i.wee@student.unsw.edu.au>
2025-10-09 17:02:50 +1100

    week 4 starter code

--- week03/node-number-manipulation-1/numbers.js
+++ week03/node-number-manipulation-1/numbers.js
@@ -1,24 +1,30 @@
 const numbers = [406, 646, 199, 996, 989, 47, 55, 614, 293, 407, 287, 605, -56, 960, 832, 25, 596, 541, -577, 56, 878, 483, 681, 17, 73, 428, -757, 923, 748, 619, 117, 588, -661, -267, 571, 95, 923, 386, 507, 243, -868, -797, 344, 660, 34, 945, -424, -169, 344, 601, 277, 478, 562, 863, 887, 172, 23, 995, 999, 2, 12, 476, 755, 617, 155, 698, 91, 1, 481, 971, 371, 164, 220, 854, 590, 364, 446, 254, 980, 469, 738, 866, 297, 410, 407, 576, 893, 319, 866, 501, 939, 536, 380, 331, 438, 76, 423, 951, 459, 425 ];
 
-let sum = 0;
-let positiveSum = 0;
-let evenSum = 0;
-let over400Sum = 0;
-let betweenSum = 0;
+// for (const num in numbers) {
+//   numbers[num]
+// }
 
+let sum = 0;
+let posNumbers = 0;
+let evenNumbers = 0;
+let above400 = 0;
+let between20and40 = 0;
 numbers.forEach((num, index) => {
   sum += num;
   if (num > 0) {
-    positiveSum += num;
+    posNumbers += num;
   }
+
   if (num % 2 === 0) {
-    evenSum += num;
+    evenNumbers += num;
   }
+
   if (num > 400) {
-    over400Sum += num;
+    above400 += num;
   }
+
   if (index >= 20 && index <= 40) {
-    betweenSum += num;
+    between20and40 += num;
   }
 });
 
@@ -38,7 +44,7 @@ Sum of numbers between indexes 20 and 40 inclusively = 5727
 
 
 console.log("Sum = " + sum);
-console.log("Positive Sum = " + positiveSum);
-console.log("Even Sum = " + evenSum);
-console.log("Sum of numbers above 400 = " + over400Sum);
-console.log("Sum of numbers between indexes 20 and 40 inclusively = " + betweenSum);
+console.log("Positive Sum =" + posNumbers);
+console.log("Even Sum = " + evenNumbers);
+console.log("Sum of numbers above 400 = " + above400);
+console.log("Sum of numbers between indexes 20 and 40 inclusively = " + between20and40);

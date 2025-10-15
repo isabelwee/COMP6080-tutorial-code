e8b4edde8829dd834d454adf28fa5724ce05b20c
Isabel Wee <i.wee@student.unsw.edu.au>
2025-10-15 18:18:26 +1100

    week 5 starter code

---
+++ week05/async-nested-promises/main.js
@@ -0,0 +1,45 @@
+const API_URL = 'https://jsonplaceholder.typicode.com'
+
+// function getPostAndUser(postId) {
+//   return fetch(`${API_URL}/posts/${postId}`)
+//     .then(res => res.json())
+//     .then(post => {
+//       return fetch(`${API_URL}/users/${post.userId}`)
+//         .then(res => res.json())
+//         .then(user => ({
+//           post,
+//           user
+//         }))
+//     })
+// }
+
+// getPostAndUser(1).then(result => {
+//   console.log(result)
+// })
+
+const getPostsPage = (pageNum) => {
+  return fetch(`${API_URL}/posts?_page=${pageNum}`)
+    .then(res => res.json())
+}
+
+function getAllPosts() {
+  let pageNum = 1;
+  const posts = [];
+
+  const handleThen = (postsPage) => {
+    if (postsPage.length <= 0) {
+      return posts
+    }
+
+    posts.push(...postsPage)
+    pageNum += 1;
+    return getPostsPage(pageNum).then(handleThen);
+  }
+
+  return getPostsPage(pageNum).then(handleThen)
+}
+
+
+getAllPosts().then(posts => {
+  console.log(posts.length)
+})

54dc2d352d48a4839c82857516fd3ac630867514
Isabel Wee <i.wee@student.unsw.edu.au>
2025-11-13 10:46:14 +1100

    starter code week9

---
+++ week09/react-ui-framework/exercise/src/App.jsx
@@ -0,0 +1,16 @@
+import { AppBar, Box, Button, Grid, Link, styled, TextField, Toolbar, Typography } from "@mui/material";
+import AirplanemodeActiveIcon from '@mui/icons-material/AirplanemodeActive';
+import beachImg from "./beach.jpeg"
+
+
+function App() {
+
+
+  return (
+    <>
+      
+    </>
+  );
+}
+
+export default App
